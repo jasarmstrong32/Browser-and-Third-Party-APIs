e74917eeb2c1514367e3e298ce47a22de549e177
jasarmstrong32 <97754601+jasarmstrong32@users.noreply.github.com>
2022-07-06 18:57:44 -0500

    Thhe page now loads 10 random images instead of 1

--- index.js
+++ index.js
@@ -10,24 +10,26 @@ function fetchImage() {
         randomImages.push(image); // pushes each image object from the response into the randomImages array
       });
 
-      const i = Math.floor(Math.random() * (99 - 0) + 1); //generates a random number between 0 and 99 to access a random index of the randomImages array.
+      for (let i = 0; i < 10; i++){
+      const j = Math.floor(Math.random() * (99 - 0) + 1); //generates a random number between 0 and 99 to access a random index of the randomImages array.
 
       //lets you access all image info via console.
-      const display = randomImages[i];
+      const display = randomImages[j];
       console.log(display);
 
       //generates the HTML needed to display the random image on the screen and adds it to the div element with the id of response.
       output += `
       <div>
-        <h2 class="center">Author: ${randomImages[i].author}</h2>
-        <p class="center">Image Id: ${randomImages[i].id}</p>
-        <p class="center">Width: ${randomImages[i].width}</p>
-        <p class="center">Height: ${randomImages[i].height}</p>
-        <p class="center"><a href="${randomImages[i].url}">Download Link!</a></p>
-        <img src="https://picsum.photos/id/${randomImages[i].id}/${randomImages[i].width}/${randomImages[i].height}" width="${randomImages[i].width}" height="${randomImages[i].height}" class="center">
+        <h2 class="center">Author: ${display.author}</h2>
+        <p class="center">Image Id: ${display.id}</p>
+        <p class="center">Width: ${display.width}</p>
+        <p class="center">Height: ${display.height}</p>
+        <p class="center"><a href="${display.url}">Download Link!</a></p>
+        <img src="https://picsum.photos/id/${display.id}/${display.width}/${display.height}" width="${display.width}" height="${display.height}" class="center">
 
       </div>
       `;
+      }
       document.getElementById("response").innerHTML = output;
     });
 }
